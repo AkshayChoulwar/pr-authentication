8c15987564537cf9ca63e1becd2c0d1486bd96dd
Akshay Choulwar <achoulwar901@gmail.com>
2020-12-08 23:11:33 +0530

    Used node mailer to send emails

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const { v4: uuidv4 } = require("uuid");
+const nodemailer = require("nodemailer");
 
 /**
  * User registration work flow.
@@ -17,14 +18,53 @@ async function registerUser(payload) {
 
       const salt = await bcrypt.genSalt(12);
       const hashPassword = await bcrypt.hash(payload.password, salt);
-      const userPayload = { ...payload, isActive: false, password: hashPassword };
+      const uuidToken = uuidv4();
+
+      const userPayload = {
+        ...payload,
+        isActive: false,
+        password: hashPassword,
+        uuidToken,
+      };
     
       const usersPayload = new User(userPayload);
-      const newUser = await usersPayload.save({
-          timestamps: true
-      })
 
-      return newUser;
+      const newUser = await usersPayload.save();
+
+      const { _id, first_name, last_name, createdAt, updatedAt } = newUser;
+
+
+      // Create Link:
+      const verifyLink = `https://localhost:7777/api/users/verifyUser?email=${newUser.email}&token=${newUser.uuidToken}`;
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD
+        },
+      });
+
+      const mailOptions = {
+        from: process.env.EMAIL,
+        to: newUser.email,
+        subject: "Confirm Product Hunt Account",
+        html: `<h4>Please click on the below link:</h4><br>${verifyLink}`,
+      };
+
+      console.log("Sending the email");
+      const emailResponse = await transporter.sendMail(mailOptions);
+
+      console.log("Email sent", emailResponse);
+
+      return {
+        userID: _id,
+        email: newUser.email,
+        first_name,
+        last_name,
+        createdAt,
+        updatedAt
+      };
+
     
     } catch (error) {
 
--- src/models/User.js
+++ src/models/User.js
@@ -1,27 +1,35 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  first_name: {
-    type: String,
-    required: true,
-    min: 2
+const userSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+      min: 2,
+    },
+    last_name: {
+      type: String,
+      required: true,
+      min: 2,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    isActive: Boolean,
+    uuidToken: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
-  last_name: {
-    type: String,
-    required: true,
-    min: 2
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 6
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  isActive: Boolean,
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
